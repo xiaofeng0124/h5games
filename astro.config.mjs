0a52116a000f73c69da2a48ba4c192d0fd2a3e96
XF012 <XF0124xf@outlook.com>
2026-05-09 00:25:41 +0800

    Update site URL from chillarcade.com to chillarcade.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://chillarcade.com',
+  site: 'https://chillarcade.io',
   output: 'static',
   trailingSlash: 'never',
 
